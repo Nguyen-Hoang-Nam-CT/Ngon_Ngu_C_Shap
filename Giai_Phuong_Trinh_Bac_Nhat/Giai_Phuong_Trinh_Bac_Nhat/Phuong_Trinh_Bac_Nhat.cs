53c13b281ad03c973b84dbf07bf6f2050c2ed20d
your name <colammoicoan10@gmail.com>
2021-09-21 19:14:22 +0700

    Thêm bài giải phương trình bậc nhất

---
+++ Giai_Phuong_Trinh_Bac_Nhat/Giai_Phuong_Trinh_Bac_Nhat/Phuong_Trinh_Bac_Nhat.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Giai_Phuong_Trinh_Bac_Nhat
+{
+    class Phuong_Trinh_Bac_Nhat
+    {
+        private int a;
+        private int b;
+        private double nghiem;
+
+        public int A
+        {
+            get
+            {
+                return a;
+            }
+            set
+            {
+                a = value;
+            }
+        }
+
+        public int B
+        {
+            get
+            {
+                return b;
+            }
+            set
+            {
+                b = value;
+            }
+        }
+        public void input()
+        {
+            Console.Write("\nNhap a: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("\nNhap b: ");
+            b = int.Parse(Console.ReadLine());
+        }
+
+    }
+}
---
+++ Giai_Phuong_Trinh_Bac_Nhat/Giai_Phuong_Trinh_Bac_Nhat/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Giai_Phuong_Trinh_Bac_Nhat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Phuong_Trinh_Bac_Nhat pt1 = new Phuong_Trinh_Bac_Nhat();
+            pt1.input();
+            if (pt1.A == 0)
+            {
+                if (pt1.B == 0)
+                    Console.Write("\nPhuong trinh co vo so nghiem");
+                else
+                    Console.Write("\nPhuong trinh vo nghiem");
+            }
+            else
+            {
+                Console.Write("\nPhuong trinh co nghiem: " + (double)(-pt1.B / pt1.A));
+            }
+            Console.ReadKey();
+        }
+    }
+}
